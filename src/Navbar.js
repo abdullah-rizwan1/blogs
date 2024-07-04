bf0f7ae49626cb495a30f56cea498a45ab4cd17c
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-07-04 12:59:18 +0500

    made changes to the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Bloggie</h1>
+      <h1>Bloggings</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
